5ad3e404373b997e1651592789fa29fb47d96af9
NafiAsib <nafi.asib@gmail.com>
2021-04-30 17:29:08 +0600

    created modules for new & past news

---
+++ src/app/modules/new-news/new-news.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NewNewsRoutingModule } from './new-news-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NewNewsRoutingModule
+  ]
+})
+export class NewNewsModule { }
