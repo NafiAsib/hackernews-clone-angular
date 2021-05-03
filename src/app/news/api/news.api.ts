11fda21518c4f8dc570e5ddae2797f75c1ecb82a
NafiAsib <nafi.asib@gmail.com>
2021-05-03 20:30:12 +0600

    api settings

--- src/app/news/api/news.api.ts
+++ src/app/news/api/news.api.ts
@@ -4,20 +4,26 @@ import { HttpClient } from '@angular/common/http';
 import { config } from '../../core/config';
 import { News } from "../models/news.model";
 import { Observable } from "rxjs";
+import { NewsDetails } from "../models/news-details.model";
 
 @Injectable()
-export class NewNewsApi {
-  readonly NEW_API = 'newstories';
-  readonly PAST_API = 'topstories'; // using topstories api, because didn't past stories api 
+export class NewsApi {
+  readonly NEW_API = '/newstories';
+  readonly PAST_API = '/topstories'; // using topstories api, because didn't past stories api 
+  readonly DETAILS_API = '/item'
 
   constructor(private http: HttpClient) {}
 
-  getNewNews(): Observable<News> {
-    return this.http.get<News>(config.apiUrl+this.NEW_API);
+  getNewNews(): Observable<News[]> {
+    return this.http.get<News[]>(config.apiUrl + this.NEW_API);
   }
 
   getPastNews(): Observable<News> {
-    return this.http.get<News>(config.apiUrl+this.PAST_API);
+    return this.http.get<News>(config.apiUrl + this.PAST_API);
+  }
+
+  getNewsDetails(id: number): Observable<NewsDetails> {
+    return this.http.get<NewsDetails>(config.apiUrl + this.DETAILS_API + '/' + id + '.json');
   }
 
 }
