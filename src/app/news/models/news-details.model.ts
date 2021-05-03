4b201ea2cc70b760c63d71cb1ee4798a76459f18
NafiAsib <nafi.asib@gmail.com>
2021-05-03 20:28:46 +0600

    model for details of a id

---
+++ src/app/news/models/news-details.model.ts
@@ -0,0 +1,11 @@
+export class NewsDetails {
+  by: String;
+  descendants: number;
+  id: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: String;
+  type: String;
+  url: String;
+}
