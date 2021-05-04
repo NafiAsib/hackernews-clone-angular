4b47d9731a09a131f34434194342316af5633092
NafiAsib <nafi.asib@gmail.com>
2021-05-04 21:32:56 +0600

    api settings fixed for past news

--- src/app/news/api/news.api.ts
+++ src/app/news/api/news.api.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 
 import { config } from '../../core/config';
-import { News } from "../models/news.model";
 import { Observable } from "rxjs";
 import { NewsDetails } from "../models/news-details.model";
 
@@ -11,7 +10,7 @@ import { NewsDetails } from "../models/news-details.model";
 })
 export class NewsApi {
   readonly NEW_API = '/newstories.json';
-  readonly PAST_API = '/topstories'; // using topstories api, because didn't past stories api 
+  readonly PAST_API = '/topstories.json'; // using topstories api, because didn't past stories api 
   readonly DETAILS_API = '/item'
 
   constructor(private http: HttpClient) {}
@@ -20,8 +19,8 @@ export class NewsApi {
     return this.http.get<number[]>(config.apiUrl + this.NEW_API);
   }
 
-  getPastNews(): Observable<News> {
-    return this.http.get<News>(config.apiUrl + this.PAST_API);
+  getPastNews(): Observable<number[]> {
+    return this.http.get<number[]>(config.apiUrl + this.PAST_API);
   }
 
   getNewsDetails(id: number): Observable<NewsDetails> {
